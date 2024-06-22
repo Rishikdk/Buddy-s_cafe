b499d9e7dac093a7c42a098bb4ef30a3f9a6a64d
Rishi Khadka <120002587+Rishikdk@users.noreply.github.com>
2024-06-22 22:11:18 +0545

    manage App for the frontent

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Components/header.jsx";
 import Desk_pic from "./assets/img/front.jpeg";
+import Popular from "./Components/Popular_Dishes.jsx";
 import "./App.css";
 
 class App extends React.Component {
@@ -10,6 +11,10 @@ class App extends React.Component {
         <div className="Containers_App">
           <div className="Container_header">
             <img src={Desk_pic} className="Desk_pic" alt="Desk_pic" />
+          </div>
+          <div className="Popular">
+            <Popular />
+            <label>Rishi khadka</label>
           </div>
         </div>
       </>
--- src/Components/Popular_Dishes.jsx
+++ src/Components/Popular_Dishes.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import Dish1 from "../assets/img/OIP.jpeg";
+import "./Style/popular_dishes.css";
 
 class Popular extends React.Component {
   render() {
     return (
       <>
-        <div className="Container__Popular">
-          <div className="Dish_img"></div>
+        <div className="Container_Popular">
+          <div className="Dish_img">
+            {/* <img src={Dish1} className="Dish_img" alt="Dish_img" /> */}
+          </div>
 
           <div className="Dish_name">
             <p>Dish Name</p>
