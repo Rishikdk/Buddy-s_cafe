bcbcd30a0abfa8214890d13b519fb7e43a26ad31
Rishi Khadka <120002587+Rishikdk@users.noreply.github.com>
2024-06-22 13:45:55 +0545

    Components files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "./Components/header.jsx";
+import Desk_pic from "./assets/img/front.jpeg";
+import "./App.css";
 
 class App extends React.Component {
   render() {
     return (
       <>
-        <div className="header">
-          <Header />
+        <div className="Containers_App">
+          <div className="Container_header">
+            <img src={Desk_pic} className="Desk_pic" alt="Desk_pic" />
+          </div>
         </div>
       </>
     );
---
+++ src/Components/Popular_Dishes.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Popular extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="Container__Popular">
+          <div className="Dish_img"></div>
+
+          <div className="Dish_name">
+            <p>Dish Name</p>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Popular;
