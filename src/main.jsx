90967cb7946c72978292f8804707eea4c58184a5
Rishi Khadka <120002587+Rishikdk@users.noreply.github.com>
2024-07-26 23:11:09 +0545

    root maping

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,25 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "@fontsource/righteous";
+import ErrorPage from "./error-page.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Menu from "./Components/Menu.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "menu",
+    element: <Menu />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
