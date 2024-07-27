fea0746ca8a0db35f55633ad6ad5cad1ff30268d
Rishi Khadka <120002587+Rishikdk@users.noreply.github.com>
2024-07-27 21:52:10 +0545

    map

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -18,9 +18,16 @@ function Footer() {
             <li>Hatti Dara,Kharey,Kathmandu</li>
             <li>Tel:9865993612</li>
             <li>info@buddys.com</li>
-            <li></li>
+            <li> </li>
             <li></li>
           </ul>
+        </div>
+        <div className="map_location">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4406.515880601209!2d85.3651113762698!3d27.69961317618705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb1b754daab30b%3A0xd76591f8bb375042!2sBuddy&#39;s%20Cafe!5e1!3m2!1sen!2snp!4v1722096051166!5m2!1sen!2snp"
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
         </div>
 
         <div className="Quick_link">
